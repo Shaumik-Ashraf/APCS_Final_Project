30e50abdd7064338f8bf69f3355ef45b4ed98ea2
bendypeanut1 <ultrasnowycashew@gmail.com>
2016-01-08 19:47:55 +0000

    Added some brainstorm code

---
+++ Ideas/Fire.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Fire extends Character{
+    
+    public Fire (ArrayList<String> player){
+        name = player.get(0);
+        element = player.get(3);
+        
+        HP = 100;
+        str = 10;
+        magic =10;
+        def = 10;
+        res = 10;
+        luck = 10;
+        speed = 10;
+   
+    }
+        
+        
+}
