8951df80e4a644c256040c209431ac0303711ce7
bendypeanut1 <ultrasnowycashew@gmail.com>
2016-01-11 19:20:57 +0000

    jcao14: Added two files that I forgot to commit yesterday. T_T

---
+++ Ideas/Monster.java
@@ -0,0 +1,21 @@
+public class Monster extends Character{
+        public Monster(){
+            element = "Wood";
+            HP = 8;
+            str = 10;
+            magic = 0;
+            def = 3;
+            res = 3;
+            luck = 1;
+            speed = 3;
+            EXP = 5;
+            
+            hpInitial = HP;
+            strInitial = str;
+            magicInitial = magic;
+            defInitial = def;
+            resInitial = res;
+            luckInitial = luck;
+            speedInitial = speed;
+        }
+}
