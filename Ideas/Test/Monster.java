84d9bdb3986e6a0a4f46e40e1eb45bd052059d5b
bendypeanut1 <ultrasnowycashew@gmail.com>
2016-01-20 19:47:08 +0000

    changed monster a bit

--- Ideas/Test/Monster.java
+++ Ideas/Test/Monster.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Monster extends GChar{
     
-    public static HashMap <String, Monster> bestiary = new HashMap <String, Monster>();
+    public static HashMap <String, String[]r> bestiary = new HashMap <String, Monster>();
     
 
     
@@ -14,7 +14,7 @@ public class Monster extends GChar{
     
     public void initMonsters()
     {
-        bestiary.put ("Pumpkin Head", new pumpkinHead() );
+        bestiary.put ("Pumpkin Head", new String[" insert mods here"] );
     }
     
     public void scale (int lvl, int exp){
@@ -30,51 +30,7 @@ public class Monster extends GChar{
         speedInitial = speed = speed * (1 + lvl/5);
     }
     
-    
-     /*
-    =================================================================================================================
-    ==========STAT METHODS===========================================================================================
-    =================================================================================================================
-    */
-    
-    
-    public boolean isAlive(){
-        return (HP > 0);
-    }
-    
-    public void statSheet(){
-        ArrayList<String> stats = new ArrayList();
-        stats.add("HP: " + HP + "/" + hpInitial);
-        stats.add("MP: " + MP + "/" + mpInitial);
-        stats.add("Str: " + str);
-        stats.add("Magic: " +magic);
-        stats.add("Def: " + def);
-        stats.add("Res: " + res);
-        stats.add("Luck: " + luck);
-        stats.add("Speed: " + speed);
-        
-        System.out.println (name);
-        System.out.println ("Level: " + level);
-        System.out.println ("Element: "+ element);
-        System.out.println ("EXP: " + EXP + "/100");
-        System.out.println (stats);
-    }
-    
-    //reset stats
-    //also use as heal (boolean healing not required; usage: class Town -> rest case)
-    //may cause conflicts when using equips
-    public void normalize(){
-       // healing = false;
-        str = strInitial;
-        magic = magicInitial;
-        def = defInitial;
-        res = resInitial;
-        luck = luckInitial;
-        speed = speedInitial;
-        crit = false;
 
-    }
-    
     
      /*
      =========================================================================================================================================================
@@ -89,30 +45,7 @@ public class Monster extends GChar{
     }
     
     
-   
-     
     
-    class pumpkinHead extends Monster{
-        public pumpkinHead ()
-        {
-            name = "Pumpkin Head";
-            element = "Wood";
-            hpInitial = HP = 20;
-            mpInitial = MP = 30;
-            strInitial = str = 13;
-            magicInitial = magic = 5;
-            defInitial = def = 4;
-            resInitial = res = 4;
-            luckInitial = luck = 3;
-            speedInitial = speed = 6;
-            EXP = 0;
-            level = 1;
-            
-            known.add("Basic Attack");
-            known.add ("Wood Spike");
-            
-        }
-    }
     
    
 }
